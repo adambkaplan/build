dab3755d6424ba3ab533de773f8d8c57270f9be6
adambkaplan <1538838+adambkaplan@users.noreply.github.com>
2025-01-16 00:19:37 +0000

    Bump gcr.io/go-containerregistry/crane from 0.19.1 to 0.20.3

--- test/v1alpha1_samples/buildstrategy_samples.go
+++ test/v1alpha1_samples/buildstrategy_samples.go
@@ -379,7 +379,7 @@ metadata:
 spec:
   buildSteps:
   - name: store-tarball
-    image: gcr.io/go-containerregistry/crane:v0.19.1
+    image: gcr.io/go-containerregistry/crane:v0.20.3
     command:
     - crane
     args:
